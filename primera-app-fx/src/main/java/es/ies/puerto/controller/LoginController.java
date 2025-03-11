15d860ce4d52b42927b133ff4cba4112d085c65e
Francisco Rodriguez Alfonso <franteleco1278@gmail.com>
2025-03-11 20:09:26 +0000

    se realiza el estilo de la pantalla de login

--- primera-app-fx/src/main/java/es/ies/puerto/controller/LoginController.java
+++ primera-app-fx/src/main/java/es/ies/puerto/controller/LoginController.java
@@ -5,20 +5,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoginController {
-    
+
     private final String usuario = "pokemon";
     private final String password = "pokemon";
 
     @FXML
     private TextField textFieldUsuario;
-    
+
     @FXML
     private PasswordField textFieldPassword;
 
@@ -26,25 +26,38 @@ public class LoginController {
     private Text textFieldMensaje;
 
     @FXML
+    private Button aceptarButton;
+    @FXML
     private Button openRegistrarButton;
 
     @FXML
     protected void onLoginButtonClick() {
 
-        if (textFieldUsuario== null || textFieldUsuario.getText().isEmpty() || 
-            textFieldPassword == null || textFieldPassword.getText().isEmpty() ) {
-                textFieldMensaje.setText("Credenciales null o vacias");
-                return;
+        if (textFieldUsuario == null || textFieldUsuario.getText().isEmpty() ||
+                textFieldPassword == null || textFieldPassword.getText().isEmpty()) {
+            textFieldMensaje.setText("Credenciales null o vacias");
+            return;
         }
 
         if (!textFieldUsuario.getText().equals(usuario) || !textFieldPassword.getText().equals(password)) {
             textFieldMensaje.setText("Credenciales invalidas");
             return;
-        } 
+        }
 
         textFieldMensaje.setText("Usuario validado correctamente");
     }
 
+    @FXML
+    public void buttonAceptarClick(MouseEvent event) {
+        aceptarButton.setStyle("-fx-background-color: #E74C3C;"); 
+    }
+    @FXML
+    public void buttonOpenRegistrarClick(MouseEvent event) {
+        aceptarButton.setStyle("-fx-background-color: #E74C3C;"); 
+    }
+    
+    
+    
     @FXML
     protected void openRegistrarClick() {
 
@@ -58,8 +71,7 @@ public class LoginController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-    
+
     }
-    
+
 }
